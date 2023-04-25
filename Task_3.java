0aafe98ae10071a2eea7209939a8a30bd492a7ce
Ilnur Garrapov <garrapovilnur@gmail.com>
2023-04-25 19:30:46 +0300

    task3

--- Task_3.java
+++ Task_3.java
@@ -3,7 +3,7 @@ import java.util.ArrayDeque;
 import java.util.Arrays;
 import java.util.Deque;
 
-public class Task_4_3 {
+public class Task_3 {
     public static void main(String[] args) {
         Deque<Integer> deque = new ArrayDeque<>(Arrays.asList(1,2,3,4,3,2,1));
         boolean res = true;
